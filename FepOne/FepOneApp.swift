81ec45aea818f5c0c0dd0e94e5b1cb77aa2f2cc8
Michael <hayrapetyanmichael1@gmail.com>
2026-01-24 21:45:34 +0400

    Initial Commit

---
+++ FepOne/FepOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FepOneApp.swift
+//  FepOne
+//
+//  Created by Michael Hayrapetyan on 24.01.26.
+//
+
+import SwiftUI
+
+@main
+struct FepOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
